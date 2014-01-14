2c2e759a5a9a24353a5aa293516e1c67e2a20252
Ivan Glushchenko <ivang.github@outlook.com>
2014-01-13 23:06:29 -0500

    first commit

---
+++ SweetHome.Core/Model.fs
@@ -0,0 +1,38 @@
+﻿namespace SweetHome.Core
+
+open System
+
+module Model =
+    
+    type FeedItem =
+        {   Id: string
+            FeedId: string
+            Url: string
+            Place: string
+            Caption: string
+            Specials: string
+            Price: float option
+            PublishedAt: DateTime
+            ReceivedAt: DateTime
+            Map: string
+            Address: string
+            Bedrooms: int option
+            Content: string
+            Images: string list
+            SimiliarItems: FeedItem list
+            SimiliarItemsTotal: int
+            SimiliarItemsToday: int
+            FirstAppearanceOffset: int
+            Phones: string list
+            Year: int }
+
+    type Feed =
+        {   Id: string
+            Name: string
+            Url: string
+            Keywords: string
+            Ignores: string
+            MinPrice: int
+            MaxPrice: int
+            Items: FeedItem list
+            IsEnabled: bool }
